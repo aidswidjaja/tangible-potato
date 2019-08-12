5cb323c243b5d2058dc50dfd44bd7b50b1167616
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2019-08-12 21:20:54 +1000

    Finished JSON fetch() script, woohoo

--- jsonfetch.js
+++ jsonfetch.js
@@ -4,37 +4,39 @@
 // https://github.com/mdn/fetch-examples/blob/master/fetch-json/index.html
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 // https://developers.google.com/web/updates/2015/03/introduction-to-fetch
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
 
 /* jshint esversion: 6*/ // yay my dreamweaver errors are gone!
 
 // Creative Commons Zero v1.0 Universal - thanks guys!
+// based off the MDN fetch() example at https://github.com/mdn/fetch-examples/blob/master/fetch-json/index.html
 
-var myList = document.querySelector('ul');
-const url = 'https://www.reddit.com/r/teenagers.json';
-   
-fetch(url)
-    .then(function(response) {
-      if (!response.ok) { // MDN implementation - Google implementation of Response Status check doesn't work haha
-        throw new Error("uh oh: Response Status = " + response.status);
-      }	
-      return response.json();
-    })
-	.then(function(json) {
-      for(var i = 0; i < json.data.length; i++) {
-        var listItem = document.createElement('li');
-        listItem.innerHTML = '<strong>' + json.data[i].modhash + '</strong>';
-        listItem.innerHTML += json.data[i].dist + '.';
-        myList.appendChild(listItem);
-      }
-    })
-    .catch(function(error) {
-      var p = document.createElement('p');
-      p.appendChild(
-        document.createTextNode('uh oh: ' + error.message)
-      );
-      document.body.insertBefore(p, myList);
-    });
+var myList = document.querySelector('p');
+var requestURL = 'https://www.reddit.com/r/teenagers.json';
 
+fetch(requestURL)
+.then(function(response) {
+  if (!response.ok) {
+    throw new Error("uh oh: HTTP error, status = " + response.status); // error checking
+  }
+  return response.json();
+})
+.then(function(json) {
+  var listItem = document.createElement('h1');
+  // get ready!
+  var contents = json.data && json.data.children && json.data.children["2"] && json.data.children["2"].data && json.data.children["2"].data.title;i
+  listItem.innerHTML = contents;
+  myList.appendChild(listItem);
+
+})
+.catch(function(error) {
+  var p = document.createElement('p');
+  p.appendChild(
+    document.createTextNode('uh oh: ' + error.message)
+  );
+  document.body.insertBefore(p, myList);
+});
 
 
 // x-moose: majestic (me when I replace dreamweaver with atom or vs code) //
